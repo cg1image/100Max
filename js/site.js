f2e7971b1517cc2837b97bb12e360c088d5e9396
CGovers <54491781+cg1image@users.noreply.github.com>
2022-07-10 10:15:02 -0700

    Creating JavaScript for 100Max code project
    Learn to create a jason file for debugger and write JavaScript to site.js file

--- js/site.js
+++ js/site.js
@@ -1,25 +1,32 @@
-
-
-
 //get the values from the Page
 //starts or controller function
 function getValues(){
     //get values from the page
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
-
-    alert("The Start Value: " + startValue);
-    
     
     //we need to call generateNumbers
+    let numbers = generateNumbers(startValue,endValue);{
+        
 
+    }
     //we call displayNumbers
 }
 
 //generate numbers from startValue to the endValue
 //logic function(s)
-function gnerateNumbers(){
+function gnerateNumbers(sValue,eValue){
 
+    let numbers = [];
+
+    //we want to get all numbers from to start to end
+
+    for (let index = sValue; index <= eValue; index++) {    
+        //this will execute in a loop until index = eValue
+        numbers.push(index);
+    }
+
+    return numbers;
 }
 
 //dispay the numbers and mark even numbers bold
